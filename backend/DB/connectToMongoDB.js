d9a0e9b90abfe79a84c79a04138c79b62b256114
wassim <wslimani455@gmail.com>
2025-02-06 21:45:54 +0100

    Backend Complited

--- backend/DB/connectToMongoDB.js
+++ backend/DB/connectToMongoDB.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
 	try {
-		await mongoose.connect("mongodb+srv://joker:OkVuOAT14LMdvdUH@cluster0.5gfzn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
-		await mongoose.connect(process.env.MONGO_DB_URI);
+		 await mongoose.connect("mongodb+srv://joker:OkVuOAT14LMdvdUH@cluster0.5gfzn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+		// await mongoose.connect(process.env.MONGO_DB_URI);
 
 		console.log("Connected to MongoDB");
 	} catch (error) {
--- backend/controller/authcontroller.js
+++ backend/controller/authcontroller.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 // import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 export const signup = async (req, res) => {
@@ -38,17 +39,20 @@ export const signup = async (req, res) => {
 
 
 
+		if (newUser) {
+			// Generate JWT token here
+			generateTokenAndSetCookie(newUser._id, res);
 			await newUser.save();
 
-		 	res.status(201).json({
+			res.status(201).json({
 				_id: newUser._id,
-		 		fullName: newUser.fullName,
-		 		username: newUser.username,
-		 		profilePic: newUser.profilePic,
-		 	});
-		//  } else {
-		//  	res.status(400).json({ error: "Invalid user data" });
-		//  }
+				fullName: newUser.fullName,
+				username: newUser.username,
+				profilePic: newUser.profilePic,
+			});
+		} else {
+			res.status(400).json({ error: "Invalid user data" });
+		}
 	} catch (error) {
 		console.log("Error in signup controller", error.message);
 		res.status(500).json({ error: "Internal Server Error" });
@@ -56,9 +60,39 @@ export const signup = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-	
+	try {
+		const { username, password } = req.body;
+		const user = await User.findOne({ username });
+		const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+
+		if (!user) {
+			return res.status(400).json({ error: "Invalid username please try again" });
+		}
+		else if( !isPasswordCorrect){
+			return res.status(400).json({ error: "Invalid   password please try again" });
+
+		}
+
+		generateTokenAndSetCookie(user._id, res);
+
+		res.status(200).json({
+			_id: user._id,
+			fullName: user.fullName,
+			username: user.username,
+			profilePic: user.profilePic,
+		});
+	} catch (error) {
+		console.log("Error in login controller", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
 };
 
 export const logout = (req, res) => {
-	
+	try {
+		res.cookie("jwt", "", { maxAge: 0 });
+		res.status(200).json({ message: "Logged out successfully" });
+	} catch (error) {
+		console.log("Error in logout controller", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
 };
